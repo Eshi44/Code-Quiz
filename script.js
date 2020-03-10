ba6b86e68184a14d91fd14336d8648ec53e21a52
Eshi44 <e.hameliervolino@gmail.com>
2020-03-10 07:34:26 -0400

    added time funtion to countdown in the top right of the page

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //Check to insure javascript file works
 //console.log("Hello World");
+var timeEl = document.getElementById("time");
+
+var secondsLeft = 76;
+//Set time
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = "Time:" + secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+  }
+
+setTime();
