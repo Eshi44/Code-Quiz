766e5d49c07dec4355a4a29f3ea8e650be004433
Eshi44 <e.hameliervolino@gmail.com>
2020-03-10 18:20:48 -0400

    added add event listener for button and added a set time function that starts the countdown from 75 was the button is clicked. Also, when button is clicked, the second page pops up and first page is hidden.Second page/question was created along with buttons

--- script.js
+++ script.js
@@ -4,9 +4,29 @@ var timeEl = document.getElementById("time");
 
 //Create event listener so that when user clicks start quiz buttton, 
 //the first-page will hide and the second page will appear
+//and the timer will start
 //Create seond-page in html
 
-var secondsLeft = 76;
+var starBtn = document.getElementById("start-Btn");
+var firstPage = document.getElementById("first-page");
+var secondPage = document.getElementById("second-page");
+var secondsLeft = 0;
+timeEl.textContent = "Time: " + secondsLeft;
+
+secondPage.style.display = "none";
+
+starBtn.addEventListener("click", function () {
+    
+    firstPage.style.display = "none";
+    secondPage.style.display = "block";
+    secondsLeft = 75; 
+    setTime();
+
+  console.log("You've clicked me!");
+    
+});
+
+
 //Set time
 function setTime() {
     var timerInterval = setInterval(function() {
@@ -20,4 +40,4 @@ function setTime() {
     }, 1000);
   }
 
-setTime();
+
