57c97ae44bc05c11a79062535685d217ddd2376b
Eshi44 <e.hameliervolino@gmail.com>
2020-03-11 20:59:04 -0400

    added for loop for incorrect question ID's and added correct/incorrect responses hidden through css.

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ var timeEl = document.getElementById("time");
 //Create seond-page in html
 
 var starBtn = document.getElementById("start-Btn");
-var ques2Answer = document.getElementById("oneBtn3Correct");
+var ques1Answer = document.getElementById("oneBtn3Correct");
+var incorrectOne = document.querySelectorAll("#oneBtn1, #oneBtn2, #oneBtn3");
+console.log(incorrectOne);
+
+var ques2Answer = document.getElementById("twoBtn3Correct");
+var ques3Answer = document.getElementById("threeBtn4Correct");
+var ques4Answer = document.getElementById("fourBtn3Correct");
+var ques5Answer = document.getElementById("fiveBtn4Correct");
 var firstPage = document.getElementById("first-page");
 var secondPage = document.getElementById("second-page");
 var thirdPage = document.getElementById("third-page");
@@ -22,6 +29,8 @@ timeEl.textContent = "Time: " + secondsLeft;
 secondPage.style.display = "none";
 thirdPage.style.display = "none";
 fourthPage.style.display = "none";
+fifthPage.style.display = "none";
+sixthPage.style.display = "none";
 
 starBtn.addEventListener("click", function () {
    
@@ -32,27 +41,73 @@ starBtn.addEventListener("click", function () {
  // console.log("You've clicked me!");  
 });
 
-ques2Answer.addEventListener("click", function () {
-   
-    secondPage.style.display = "none";
-    thirdPage.style.display = "block";
-  //console.log("You've clicked me!");  
-});
-
-
-
 
 //Set time
 function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = "Time:" + secondsLeft;
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-      }
-  
-    }, 1000);
-  }
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "Time:" + secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
+}
 
 
+//User clicks on correct answer for question 1
+ques1Answer.addEventListener("click", function () {
+  document.getElementById("correct-answer").style.display = "block";
+  setTimeout(goToNextPage, 2000);
+      
+    
+// //console.log("You've clicked me!");  
+});
+
+
+for (var i=0; i<incorrectOne.length; i++){
+  incorrectOne[i].addEventListener("click", function(){
+    document.getElementById("incorrect-answer").style.display = "block";
+    setTimeout(goToNextPage, 2000);
+  });
+
+//console.log("You've clicked me!");  
+};
+
+  
+function goToNextPage() {
+  document.getElementById("correct-answer").style.display = "none";
+  document.getElementById("incorrect-answer").style.display = "none";
+
+  secondPage.style.display = "none";
+  thirdPage.style.display = "block";
+
+}
+
+
+// question2.addEventListener("click", function () {
+   
+//   thirdPage.style.display = "none";
+//   fourthPage.style.display = "block";
+// //console.log("You've clicked me!");  
+// });
+
+// question3.addEventListener("click", function () {
+   
+//   fourthPage.style.display = "none";
+//   fifthPage.style.display = "block";
+// //console.log("You've clicked me!");  
+// });
+
+// question4.addEventListener("click", function () {
+   
+//   fifthPage.style.display = "none";
+//   sixthPage.style.display = "block";
+// //console.log("You've clicked me!");  
+// });
+
+
+
+
+      
